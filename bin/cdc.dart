9b8a334dc5c4985acb4557c4f0e965f9920b1387
Thomas Texier <thomas_texier@connect-tech.sncf>
2024-02-09 08:51:08 +0100

    [INIT] Init project setup

---
+++ bin/cdc.dart
@@ -0,0 +1,5 @@
+import 'package:cdc/cdc.dart' as cdc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cdc.calculate()}!');
+}
---
+++ test/cdc_test.dart
@@ -0,0 +1,8 @@
+import 'package:cdc/cdc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
