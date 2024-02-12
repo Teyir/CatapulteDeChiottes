fb6eebf519a7d91e730442c87d484f6feea4e5f1
HowBaka <gravy.thom@gmail.com>
2024-02-12 19:58:02 +0100

    [INIT] Add hour to door interface and spy

--- test/utils/door_spy_test.dart
+++ test/utils/door_spy_test.dart
@@ -3,12 +3,28 @@ import 'package:cdc/interfaces/door_interface.dart';
 class DoorSpy implements IDoor {
   bool _isOpenRequested = false;
 
+  @override
+  int beginHour;
+
+  @override
+  int endHour;
+
+  DoorSpy({
+    required this.beginHour,
+    required this.endHour,
+  });
+
   bool isOpenRequested() {
     return _isOpenRequested;
   }
 
   @override
   void open() {
-    _isOpenRequested = true;
+    DateTime now = DateTime.now();
+    if (now.hour <= beginHour || now.hour >= endHour) {
+      _isOpenRequested = false;
+    } else {
+      _isOpenRequested = true;
+    }
   }
 }
