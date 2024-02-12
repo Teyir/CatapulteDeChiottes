938dc91657e61d32af7ff6990f0ee84c83c60ba2
HowBaka <gravy.thom@gmail.com>
2024-02-12 19:58:43 +0100

    [IMPROVE] Test hour check on door

--- test/security_test.dart
+++ test/security_test.dart
@@ -10,7 +10,7 @@ void main() {
   //This test emulate a simple use case with a fake reader and a door spy.
   //We are checking if the door is opened when the badge is detected.
   test('BasicCheck', () {
-    final DoorSpy doorSpy = DoorSpy();
+    final DoorSpy doorSpy = DoorSpy(beginHour: 0, endHour: 24);
     final FakeReader fakeReader = FakeReader([doorSpy]);
     final Badge badge = Badge(isRestricted: false);
 
@@ -24,7 +24,7 @@ void main() {
   //This test emulate a simple use case with a fake reader and a door spy.
   //We are checking if the door is not opened when the badge is restricted..
   test('BasicCheckWithRestrictedBadge', () {
-    final DoorSpy doorSpy = DoorSpy();
+    final DoorSpy doorSpy = DoorSpy(beginHour: 0, endHour: 24);
     final FakeReader fakeReader = FakeReader([doorSpy]);
     final Badge badge = Badge(isRestricted: true);
 
@@ -38,7 +38,7 @@ void main() {
   // This test emulate a simple use case with a fake reader and a door spy.
   // We are checking if the door keep locked when we are not checking the reader.
   test('BasicCheckWithoutCheckingReader', () {
-    final DoorSpy doorSpy = DoorSpy();
+    final DoorSpy doorSpy = DoorSpy(beginHour: 0, endHour: 24);
     final FakeReader fakeReader = FakeReader([doorSpy]);
 
     EngineManager.callReaders([fakeReader]);
@@ -49,7 +49,7 @@ void main() {
   // This test emulate a simple use case with a fake reader and a door spy.
   // We are check if the door keep locked when we are not using the badge reader.
   test('BasicCheckWithoutEmulatingBadgeDetection', () {
-    final DoorSpy doorSpy = DoorSpy();
+    final DoorSpy doorSpy = DoorSpy(beginHour: 0, endHour: 24);
     final FakeReader fakeReader = FakeReader([doorSpy]);
     final Badge badge = Badge(isRestricted: false);
 
@@ -61,8 +61,8 @@ void main() {
   // This test emulate a simple use case with a fake reader and 2 doors spy.
   //We are checking if the doors are opened when the badge is detected.
   test('BasicCheckWithMultipleDoors', () {
-    final DoorSpy doorSpy1 = DoorSpy();
-    final DoorSpy doorSpy2 = DoorSpy();
+    final DoorSpy doorSpy1 = DoorSpy(beginHour: 0, endHour: 24);
+    final DoorSpy doorSpy2 = DoorSpy(beginHour: 0, endHour: 24);
     final FakeReader fakeReader = FakeReader([doorSpy1, doorSpy2]);
     final Badge badge = Badge(isRestricted: false);
 
@@ -77,7 +77,7 @@ void main() {
   // This test emulate a simple use case with two fakes readers and a door spy.
   // We are checking if the door is opened when the badge is detected in one reader.
   test('BasicCheckWithMultipleReaders', () {
-    final DoorSpy doorSpy = DoorSpy();
+    final DoorSpy doorSpy = DoorSpy(beginHour: 0, endHour: 24);
     final FakeReader fakeReader1 = FakeReader([doorSpy]);
     final FakeReader fakeReader2 = FakeReader([doorSpy]);
     final Badge badge = Badge(isRestricted: false);
@@ -92,8 +92,8 @@ void main() {
   // This test emulate a use case with two fakes readers and two doors spy.
   // We are emulating a door opening (door 2), and we are checking if the door 1 is not opened.
   test('MultipleReadersAndMultipleDoors', () {
-    final DoorSpy doorSpy1 = DoorSpy();
-    final DoorSpy doorSpy2 = DoorSpy();
+    final DoorSpy doorSpy1 = DoorSpy(beginHour: 0, endHour: 24);
+    final DoorSpy doorSpy2 = DoorSpy(beginHour: 0, endHour: 24);
     final FakeReader fakeReader1 = FakeReader([doorSpy1]);
     final FakeReader fakeReader2 = FakeReader([doorSpy2]);
     final Badge badge = Badge(isRestricted: false);
@@ -104,5 +104,33 @@ void main() {
 
     expect(doorSpy1.isOpenRequested(), false);
     expect(doorSpy2.isOpenRequested(), true);
+  });
+
+  // This test emulate a simple use case with a fake reader and a door spy.
+  // We are check if the door is opening when the badge is between his working hours.
+  test('BasicDoorCheckWhenInWorkingHours', () {
+    final DoorSpy doorSpy = DoorSpy(beginHour: 8, endHour: 17);
+    final FakeReader fakeReader = FakeReader([doorSpy]);
+    final Badge badge = Badge(isRestricted: false);
+
+    fakeReader.emulateBadgeDetection(badge);
+
+    EngineManager.callReaders([fakeReader]);
+
+    expect(doorSpy.isOpenRequested(), true);
+  });
+
+  // This test emulate a simple use case with a fake reader and a door spy.
+  // We are check if the door keep locked when the badge is not between his working hours.
+  test('BasicDoorCheckWhenNotInWorkingHours', () {
+    final DoorSpy doorSpy = DoorSpy(beginHour: 0, endHour: 1);
+    final FakeReader fakeReader = FakeReader([doorSpy]);
+    final Badge badge = Badge(isRestricted: false);
+
+    fakeReader.emulateBadgeDetection(badge);
+
+    EngineManager.callReaders([fakeReader]);
+
+    expect(doorSpy.isOpenRequested(), false);
   });
 }
